5b632a470345f332e6fb262e9a6c5cf7f2cc4570
Adam La Rosa <adam.larosa@gmail.com>
2020-03-31 12:05:44 -0700

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,11 @@
-# Your code goes here!
+class Anagram
+	attr_accessor :word
+
+	def initialize(word)
+	    @word = word
+	end
+
+	def match(array)
+	    array.delete_if {|a| @word.split("").sort != a.split("").sort}
+	end
+end
